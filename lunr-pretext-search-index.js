5ac0f24bb8c952f5878b8a3713709123df58b03f
TienChih <tien.chih@emory.edu>
2023-07-28 20:27:21 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,418 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   tienchih.github.io   https:\/\/tienchih.github.io   copyright  "
+},
+{
+  "id": "section-set",
+  "level": "1",
+  "url": "section-set.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "SET",
+  "body": " SET  Each SET card has four characteristics.    Color : Red, Green or Purple.     Shading : Bold, Empty or Striped.     Number : One, Two or Three shapes.     Shape : Oval, Diamonds, or Squiggle.        The SET cards      A SET is a collection of 3 cards such that for each of the four characteristics, either:   All three cards share the same value. OR     All three cards have different values.         Verify that the following are SETs.                       (Go through each of the four characteristics, Color, Shading, Number, Shape. For the three cards, do all three cards have the same value or do all three have different values? Repeat for the remaining characteristics.)     Explain why the following are NOT SETs.                    What sets can you find here?   12 SET cards.    Repeat with the Daily SET Puzzle.     Play SET!     For each of the following, determine what card(s) if any could be added to form a SET:               Pick any two cards, can you find what card(s) if any will complete a set? Repeat.    For any two cards, is it always possible to find a third card to complete a SET? How many options do you have? Justify your assertions.    Pick any three cards who do not form a set and arrange them in a corner of a grid like so:  Fill out this grid so that each row, column and diagonal forms a SET.  For each pair of cards on this grid, is there a third card also on this grid which forms a SET?    For the grid you constructed in , how would you describe the position of the sets in relation to the grid?   "
+},
+{
+  "id": "set_cards",
+  "level": "2",
+  "url": "section-set.html#set_cards",
+  "type": "Figure",
+  "number": "1.1.1",
+  "title": "",
+  "body": "    The SET cards  "
+},
+{
+  "id": "def-SET",
+  "level": "2",
+  "url": "section-set.html#def-SET",
+  "type": "Definition",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  A SET is a collection of 3 cards such that for each of the four characteristics, either:   All three cards share the same value. OR     All three cards have different values.      "
+},
+{
+  "id": "activity-1",
+  "level": "2",
+  "url": "section-set.html#activity-1",
+  "type": "Activity",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  Verify that the following are SETs.                       (Go through each of the four characteristics, Color, Shading, Number, Shape. For the three cards, do all three cards have the same value or do all three have different values? Repeat for the remaining characteristics.)  "
+},
+{
+  "id": "activity-2",
+  "level": "2",
+  "url": "section-set.html#activity-2",
+  "type": "Activity",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Explain why the following are NOT SETs.                  "
+},
+{
+  "id": "activity-3",
+  "level": "2",
+  "url": "section-set.html#activity-3",
+  "type": "Activity",
+  "number": "1.1.3",
+  "title": "",
+  "body": " What sets can you find here?   12 SET cards.    Repeat with the Daily SET Puzzle.   "
+},
+{
+  "id": "activity-4",
+  "level": "2",
+  "url": "section-set.html#activity-4",
+  "type": "Activity",
+  "number": "1.1.4",
+  "title": "",
+  "body": " Play SET!  "
+},
+{
+  "id": "activity-5",
+  "level": "2",
+  "url": "section-set.html#activity-5",
+  "type": "Activity",
+  "number": "1.1.5",
+  "title": "",
+  "body": "  For each of the following, determine what card(s) if any could be added to form a SET:               Pick any two cards, can you find what card(s) if any will complete a set? Repeat.  "
+},
+{
+  "id": "problem-1",
+  "level": "2",
+  "url": "section-set.html#problem-1",
+  "type": "Problem",
+  "number": "1.1.4",
+  "title": "",
+  "body": " For any two cards, is it always possible to find a third card to complete a SET? How many options do you have? Justify your assertions.  "
+},
+{
+  "id": "activity-buildplane",
+  "level": "2",
+  "url": "section-set.html#activity-buildplane",
+  "type": "Activity",
+  "number": "1.1.6",
+  "title": "",
+  "body": " Pick any three cards who do not form a set and arrange them in a corner of a grid like so:  Fill out this grid so that each row, column and diagonal forms a SET.  For each pair of cards on this grid, is there a third card also on this grid which forms a SET?  "
+},
+{
+  "id": "problem-2",
+  "level": "2",
+  "url": "section-set.html#problem-2",
+  "type": "Problem",
+  "number": "1.1.5",
+  "title": "",
+  "body": " For the grid you constructed in , how would you describe the position of the sets in relation to the grid?  "
+},
+{
+  "id": "section-plane",
+  "level": "1",
+  "url": "section-plane.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Neutral Plane Axioms",
+  "body": " Neutral Plane Axioms   Introducing Axiom Systems and Models    An axiom system has two components:   A set of special words that we officially assume have no meaning. The words are sometimes called undefined terms, primitives or technical terms.  A set of statements called axioms that we assume to be true concerning the undefined terms.       A model of an axiom system is a set of meanings for the undefined terms. These meanings are assigned without any regard for whether the axioms become true statements about the undefined terms.     Consider the following axiom system, the 4 point geometry . Let point , line and belongs be the undefined terms. The axioms are:   There are 4 points.    Given two distinct points, there is exactly one line that they both belong to.    Each line has exactly two points belonging to it.   Then the following are models of this axiom system.    Points are letters, lines are columns, a point belongs a line if it is on the column:     Points are dots, segments are lines, point belongs to a line if a segment connects them:  A model for a 4-point geometry   A complete graph on 4 vertices.           Come up with your own model for the 4 point geometry.    Exaplain why the following is not a model for the 4 point geometry    Compare your model to everyone else's models and the examples in . Are these models actually different?      Neutral Plane Axioms  We consider the following axiom system. Let point , line and belongs be the undefined terms.    For each line , there are at least two distinct points belonging to .      For each line , then there is at least one point not belonging to .      There exists at least one line.     Would any model for the 4-point geometry also be a model for this axiom system? Justify your assertion.    What is the minimum number of points possible for a model of this axiom system? Justify your assertion.    Find a model that satisfies , and but not .    Find a model that satisfies , and in which at least two points belong to no line.    Prove or disprove: Each point belongs to a line.   Since such a geometry is fundamentally uninteresting, we add a fourth axiom.    For each pair of distinct points, there is at least one line they both belong to.     Let cards be points and SETs be lines. Does the grid you constructed in satisfy all four of our axioms? Justify your assertion.     Find the invalid line of the following proof .     By , there is a line , by , has two points, , . So for each point , there is a line containing .    Prove that every point belongs to a line.    Is it possible that some point belongs to only one line? Prove your response.    Find a model that satisfies our axiom system in which there are two different points and such that at least two different lines each contain both and .    Find a model that satisfies our axiom system in which there are two different points and such that at least two different lines each contain both and .   Since this axiom system still seems flawed, we replace the fourth axiom with a stronger version.    For each pair of distinct points, there is exactly one line they both belong to.     We let denote the line containing both and .    A collection of points are colinear if there are is a line which all the points in the collection belong to.    What would we mean by the statement that points are non-collinear?    Is it true that given a point that there exist two more points such that the three points are collinear? Prove your answer.    Can we say each point is not in some line? Prove your answer.    Suppose and are different lines. What can you say about the number of points that can belong to both and ? Prove your answer.     If are lines, and a point belongs to both lines, we say and  intersect .      Parallelism    Let points be ordered pairs such that are real numbers. Let lines by sets of points which satisfy the equality for some . (This should be familiar).    Show that this model satisfies our axioms.    Let be a line and let be a point which doesn't belong to . How many lines are there that contain but don't intersect .      Let points be ordered pairs such that are real numbers and  . Let lines by sets of points which satisfy the equality for some  and  . (This may not be familiar).       Show that this model satisfies our axioms.    Let be a line and let be a point which doesn't belong to . How many lines are there that contain but don't intersect .     Find a model for our axioms where every pair of lines intersect.    For any model of our axioms, let be a line and let be a point which doesn't belong to . What can we say about the number of lines which contain but don't intersect .    "
+},
+{
+  "id": "def-geometry",
+  "level": "2",
+  "url": "section-plane.html#def-geometry",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  An axiom system has two components:   A set of special words that we officially assume have no meaning. The words are sometimes called undefined terms, primitives or technical terms.  A set of statements called axioms that we assume to be true concerning the undefined terms.       A model of an axiom system is a set of meanings for the undefined terms. These meanings are assigned without any regard for whether the axioms become true statements about the undefined terms.   "
+},
+{
+  "id": "example-1",
+  "level": "2",
+  "url": "section-plane.html#example-1",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": " Consider the following axiom system, the 4 point geometry . Let point , line and belongs be the undefined terms. The axioms are:   There are 4 points.    Given two distinct points, there is exactly one line that they both belong to.    Each line has exactly two points belonging to it.   Then the following are models of this axiom system.    Points are letters, lines are columns, a point belongs a line if it is on the column:     Points are dots, segments are lines, point belongs to a line if a segment connects them:  A model for a 4-point geometry   A complete graph on 4 vertices.        "
+},
+{
+  "id": "problem-3",
+  "level": "2",
+  "url": "section-plane.html#problem-3",
+  "type": "Problem",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  Come up with your own model for the 4 point geometry.    Exaplain why the following is not a model for the 4 point geometry    Compare your model to everyone else's models and the examples in . Are these models actually different?   "
+},
+{
+  "id": "axiom-linesize",
+  "level": "2",
+  "url": "section-plane.html#axiom-linesize",
+  "type": "Axiom",
+  "number": "1.2.5",
+  "title": "",
+  "body": "  For each line , there are at least two distinct points belonging to .   "
+},
+{
+  "id": "axiom-notbelong",
+  "level": "2",
+  "url": "section-plane.html#axiom-notbelong",
+  "type": "Axiom",
+  "number": "1.2.6",
+  "title": "",
+  "body": "  For each line , then there is at least one point not belonging to .   "
+},
+{
+  "id": "axiom-existsline",
+  "level": "2",
+  "url": "section-plane.html#axiom-existsline",
+  "type": "Axiom",
+  "number": "1.2.7",
+  "title": "",
+  "body": "  There exists at least one line.   "
+},
+{
+  "id": "problem-4",
+  "level": "2",
+  "url": "section-plane.html#problem-4",
+  "type": "Problem",
+  "number": "1.2.8",
+  "title": "",
+  "body": " Would any model for the 4-point geometry also be a model for this axiom system? Justify your assertion.  "
+},
+{
+  "id": "problem-5",
+  "level": "2",
+  "url": "section-plane.html#problem-5",
+  "type": "Problem",
+  "number": "1.2.9",
+  "title": "",
+  "body": " What is the minimum number of points possible for a model of this axiom system? Justify your assertion.  "
+},
+{
+  "id": "problem-6",
+  "level": "2",
+  "url": "section-plane.html#problem-6",
+  "type": "Problem",
+  "number": "1.2.10",
+  "title": "",
+  "body": " Find a model that satisfies , and but not .  "
+},
+{
+  "id": "problem-7",
+  "level": "2",
+  "url": "section-plane.html#problem-7",
+  "type": "Problem",
+  "number": "1.2.11",
+  "title": "",
+  "body": " Find a model that satisfies , and in which at least two points belong to no line.  "
+},
+{
+  "id": "problem-8",
+  "level": "2",
+  "url": "section-plane.html#problem-8",
+  "type": "Problem",
+  "number": "1.2.12",
+  "title": "",
+  "body": " Prove or disprove: Each point belongs to a line.  "
+},
+{
+  "id": "axiom-twopointweak",
+  "level": "2",
+  "url": "section-plane.html#axiom-twopointweak",
+  "type": "Axiom",
+  "number": "1.2.13",
+  "title": "",
+  "body": "  For each pair of distinct points, there is at least one line they both belong to.   "
+},
+{
+  "id": "problem-9",
+  "level": "2",
+  "url": "section-plane.html#problem-9",
+  "type": "Problem",
+  "number": "1.2.14",
+  "title": "",
+  "body": " Let cards be points and SETs be lines. Does the grid you constructed in satisfy all four of our axioms? Justify your assertion.  "
+},
+{
+  "id": "problem-10",
+  "level": "2",
+  "url": "section-plane.html#problem-10",
+  "type": "Problem",
+  "number": "1.2.15",
+  "title": "",
+  "body": "  Find the invalid line of the following proof .   "
+},
+{
+  "id": "proof-1",
+  "level": "2",
+  "url": "section-plane.html#proof-1",
+  "type": "Proof",
+  "number": "1.2.2.1",
+  "title": "",
+  "body": " By , there is a line , by , has two points, , . So for each point , there is a line containing .  "
+},
+{
+  "id": "problem-11",
+  "level": "2",
+  "url": "section-plane.html#problem-11",
+  "type": "Problem",
+  "number": "1.2.16",
+  "title": "",
+  "body": " Prove that every point belongs to a line.  "
+},
+{
+  "id": "problem-12",
+  "level": "2",
+  "url": "section-plane.html#problem-12",
+  "type": "Problem",
+  "number": "1.2.17",
+  "title": "",
+  "body": " Is it possible that some point belongs to only one line? Prove your response.  "
+},
+{
+  "id": "problem-13",
+  "level": "2",
+  "url": "section-plane.html#problem-13",
+  "type": "Problem",
+  "number": "1.2.18",
+  "title": "",
+  "body": " Find a model that satisfies our axiom system in which there are two different points and such that at least two different lines each contain both and .  "
+},
+{
+  "id": "problem-14",
+  "level": "2",
+  "url": "section-plane.html#problem-14",
+  "type": "Problem",
+  "number": "1.2.19",
+  "title": "",
+  "body": " Find a model that satisfies our axiom system in which there are two different points and such that at least two different lines each contain both and .  "
+},
+{
+  "id": "axiom-twopoint",
+  "level": "2",
+  "url": "section-plane.html#axiom-twopoint",
+  "type": "Axiom",
+  "number": "1.2.20",
+  "title": "",
+  "body": "  For each pair of distinct points, there is exactly one line they both belong to.   "
+},
+{
+  "id": "definition-3",
+  "level": "2",
+  "url": "section-plane.html#definition-3",
+  "type": "Definition",
+  "number": "1.2.21",
+  "title": "",
+  "body": " We let denote the line containing both and .  "
+},
+{
+  "id": "definition-4",
+  "level": "2",
+  "url": "section-plane.html#definition-4",
+  "type": "Definition",
+  "number": "1.2.22",
+  "title": "",
+  "body": " A collection of points are colinear if there are is a line which all the points in the collection belong to.  "
+},
+{
+  "id": "problem-15",
+  "level": "2",
+  "url": "section-plane.html#problem-15",
+  "type": "Problem",
+  "number": "1.2.23",
+  "title": "",
+  "body": " What would we mean by the statement that points are non-collinear?  "
+},
+{
+  "id": "problem-16",
+  "level": "2",
+  "url": "section-plane.html#problem-16",
+  "type": "Problem",
+  "number": "1.2.24",
+  "title": "",
+  "body": " Is it true that given a point that there exist two more points such that the three points are collinear? Prove your answer.  "
+},
+{
+  "id": "problem-17",
+  "level": "2",
+  "url": "section-plane.html#problem-17",
+  "type": "Problem",
+  "number": "1.2.25",
+  "title": "",
+  "body": " Can we say each point is not in some line? Prove your answer.  "
+},
+{
+  "id": "problem-18",
+  "level": "2",
+  "url": "section-plane.html#problem-18",
+  "type": "Problem",
+  "number": "1.2.26",
+  "title": "",
+  "body": " Suppose and are different lines. What can you say about the number of points that can belong to both and ? Prove your answer.  "
+},
+{
+  "id": "def-intersect",
+  "level": "2",
+  "url": "section-plane.html#def-intersect",
+  "type": "Definition",
+  "number": "1.2.27",
+  "title": "",
+  "body": "  If are lines, and a point belongs to both lines, we say and  intersect .   "
+},
+{
+  "id": "problem-19",
+  "level": "2",
+  "url": "section-plane.html#problem-19",
+  "type": "Problem",
+  "number": "1.2.28",
+  "title": "",
+  "body": "  Let points be ordered pairs such that are real numbers. Let lines by sets of points which satisfy the equality for some . (This should be familiar).    Show that this model satisfies our axioms.    Let be a line and let be a point which doesn't belong to . How many lines are there that contain but don't intersect .   "
+},
+{
+  "id": "problem-20",
+  "level": "2",
+  "url": "section-plane.html#problem-20",
+  "type": "Problem",
+  "number": "1.2.29",
+  "title": "",
+  "body": "  Let points be ordered pairs such that are real numbers and  . Let lines by sets of points which satisfy the equality for some  and  . (This may not be familiar).       Show that this model satisfies our axioms.    Let be a line and let be a point which doesn't belong to . How many lines are there that contain but don't intersect .   "
+},
+{
+  "id": "problem-21",
+  "level": "2",
+  "url": "section-plane.html#problem-21",
+  "type": "Problem",
+  "number": "1.2.30",
+  "title": "",
+  "body": " Find a model for our axioms where every pair of lines intersect.  "
+},
+{
+  "id": "problem-22",
+  "level": "2",
+  "url": "section-plane.html#problem-22",
+  "type": "Problem",
+  "number": "1.2.31",
+  "title": "",
+  "body": " For any model of our axioms, let be a line and let be a point which doesn't belong to . What can we say about the number of lines which contain but don't intersect .  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
